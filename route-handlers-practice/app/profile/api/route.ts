ab20876982d366ca13723108583bf5dabf65bcc7
Prajwal Acharya <prajwal9498@gmail.com>
2024-09-18 12:02:21 +0545

    headers, caching route handlers and Middlewares

---
+++ route-handlers-practice/app/profile/api/route.ts
@@ -0,0 +1,26 @@
+import { type NextRequest } from "next/server";
+import { headers, cookies } from "next/headers";
+
+export async function GET(request: NextRequest) {
+  const requestHeaders = new Headers(request.headers);
+  const headerList = headers();
+
+  // cookies().set("resultsPerPage", "20");
+
+  const theme = request.cookies.get("theme");
+
+  console.log(headerList.get("Authorization"));
+  console.log(requestHeaders.get("Authorization"));
+
+  console.log(theme);
+  //   console.log(cookies().get("resultsPerPage"));
+  //   cookies().delete("resultsPerPage");
+  cookies().set("resultsPerPage", "", { maxAge: -1 });
+
+  return new Response("<h1>Profile API data</h1>", {
+    headers: {
+      "Content-Type": "text/html",
+      "Set-Cookie": "theme=dark",
+    },
+  });
+}
